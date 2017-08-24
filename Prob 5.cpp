381c66b82e6d7f6ac96307dafc7c962a16646f48
samapikart <31254492+samapikart@users.noreply.github.com>
2017-08-25 00:35:29 +0530

    Create Prob 5.cpp

---
+++ Prob 5.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+int main() {
+
+	  int l, b, area;
+
+  cout << "Enters length and breadth : "<<endl;
+
+  cin >> l >> b;
+
+  
+
+  area = l * b;
+
+  cout << "The area is " << area;
+
+ 	return 0;
+
+}
