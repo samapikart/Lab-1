e05701f9d9a136d36921f8825a67f41942c16381
samapikart <31254492+samapikart@users.noreply.github.com>
2017-08-25 00:30:01 +0530

    Create Prob 1.cpp

---
+++ Prob 1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	bool a;
+	cin >> a;
+	cout << a<<endl;
+	
+	int b;
+	cin >> b;
+	cout << b<<endl;
+	
+	double c;
+	cin >> c;
+	cout << c<<endl ;
+	
+	char d;
+	cin >> d;
+	cout << d<<endl ;
+	
+	return 0;
+}
