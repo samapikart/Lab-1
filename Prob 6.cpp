ae1a04a47642a0987aa741b694f43bfd12c61293
samapikart <31254492+samapikart@users.noreply.github.com>
2017-08-25 00:36:13 +0530

    Create Prob 6.cpp

---
+++ Prob 6.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+int main() 
+
+{
+
+	  float r, diameter, circumference, area;
+
+  cout << "Enter radius of the circle : "<<endl;
+
+  cin >> r;
+
+  
+
+  diameter = 2 * r;
+
+  cout << "The diameter is " << diameter<<endl;
+
+  
+
+  circumference = 2 * 3.14 * r;
+
+  cout << "The diameter is " << circumference<<endl;
+
+  
+
+  area = 3.14 * r * r ;
+
+  cout << " The area is " << area;
+
+  
+
+  return 0;
+
+}
