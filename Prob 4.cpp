f7685a987bfcebb5982d1d332abc654bd8654ab5
samapikart <31254492+samapikart@users.noreply.github.com>
2017-08-25 00:34:50 +0530

    Create Prob 4.cpp

---
+++ Prob 4.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+int main() {
+
+	  int l, b, perimeter;
+
+  cout << "Enters length and breadth : "<<endl;
+
+  cin >> l >> b;
+
+  
+
+  perimeter = 2 * ( l + b );
+
+  cout << "The perimeter is " << perimeter<<endl;
+
+ 	return 0;
+
+}
