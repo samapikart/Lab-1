f11c8a729db97685ec693be08ce66768ea2746ce
samapikart <31254492+samapikart@users.noreply.github.com>
2017-08-25 00:33:13 +0530

    Create Prob 2.cpp

---
+++ Prob 2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+int main() {
+
+  int a, b, sum;
+
+  cout << "Enters two numbers to find their sum : "<<endl;
+
+  cin >> a >> b;
+
+  sum = a + b;
+
+  cout << "The sum is " << sum;
+
+}
