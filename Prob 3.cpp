d89950a8ea549f8d264e98227c86819c9e2b6ea0
samapikart <31254492+samapikart@users.noreply.github.com>
2017-08-25 00:34:08 +0530

    Create Prob 3.cpp

---
+++ Prob 3.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+int main() {
+
+	  int a, b, sum, subtract, multiply, divide;
+
+  cout << "Enters two numbers : "<<endl;
+
+  cin >> a >> b;
+
+  
+
+  sum = a + b;
+
+  cout << "The sum is " << sum<<endl;
+
+  
+
+  subtract = a - b;
+
+  cout << "The subtraction is " << subtract<<endl;
+
+  
+
+  multiply = a * b;
+
+  cout << "The multiplication is " << multiply<<endl;
+
+  
+
+  divide = a / b;
+
+  cout << "The division is " << divide;
+
+ 
+
+}
